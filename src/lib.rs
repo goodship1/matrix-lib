d4f03196c381ca9d800dbc9bd698011918b75383
Gavin Goodship <msgoodship1@gmail.com>
2023-05-27 22:02:39 +0100

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,8 @@
-mod matrix;
-mod block;
-mod sparse;
-mod banded;
-mod operations;
+pub mod matrix;
+//mod block;
+//mod sparse;
+//mod banded;
+//mod operations;
 
 pub use matrix::Matrix;
-pub use block::BlockMatrix;
-pub use sparse::SparseMatrix;
-pub use banded::BandedMatrix;
-pub use operations::Operations;
+
