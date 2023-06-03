757ac75013115ab552c3f2643c7de2978a6b2286
Gavin Goodship <msgoodship1@gmail.com>
2023-06-03 22:31:57 +0100

    Update sparse.rs

--- src/sparse.rs
+++ src/sparse.rs
@@ -15,33 +15,34 @@ impl SparseMatrix {
         }
     }
 
+    pub fn iter_nonzero(&self) -> impl Iterator<Item = (usize, usize, f64)> + '_ {
+        self.elements
+            .iter()
+            .map(|((row, col), &value)| (*row, *col, value))
+    }
+
     pub fn set(&mut self, row: usize, col: usize, value: f64) {
-        assert!(row < self.rows && col < self.cols, "Index out of bounds");
+        assert!(
+            row < self.rows && col < self.cols,
+            "Index out of bounds"
+        );
         self.elements.insert((row, col), value);
     }
 
     pub fn get(&self, row: usize, col: usize) -> f64 {
-        if let Some(&value) = self.elements.get(&(row, col)) {
-            value
-        } else {
-            0.0
-        }
+        *self.elements.get(&(row, col)).unwrap_or(&0.0)
     }
 
     pub fn to_dense(&self) -> Vec<Vec<f64>> {
         let mut dense = vec![vec![0.0; self.cols]; self.rows];
         for ((row, col), &value) in self.elements.iter() {
-            dense[row][col] = value;
+            dense[*row][*col] = value;
         }
         dense
     }
-    
+
     pub fn triangle_lower(rows: usize, data: Vec<f64>) -> Self {
-        let mut new_matrix = SparseMatrix {
-            rows,
-            cols: rows,
-            elements: HashMap::new(),
-        };
+        let mut new_matrix = SparseMatrix::new(rows, rows);
 
         for x in 0..rows {
             for y in 0..=x {
@@ -56,16 +57,12 @@ impl SparseMatrix {
         new_matrix
     }
 
-    pub fn triangle_above(rows: usize, data: Vec<f64>) -> Self {
-        let mut new_matrix = SparseMatrix {
-            rows,
-            cols: rows,
-            elements: HashMap::new(),
-        };
+    pub fn triangle_upper(rows: usize, data: Vec<f64>) -> Self {
+        let mut new_matrix = SparseMatrix::new(rows, rows);
 
         for x in 0..rows {
             for y in x..rows {
-                let index = x * rows + y;
+                let index = x * rows + y - (x * (x + 1)) / 2;
                 let value = data.get(index).cloned().unwrap_or(0.0);
                 if value != 0.0 {
                     new_matrix.elements.insert((x, y), value);
@@ -75,15 +72,11 @@ impl SparseMatrix {
 
         new_matrix
     }
-    
+
     pub fn tridiagonal(rows: usize, data: Vec<f64>) -> Self {
         assert_eq!(data.len(), 3 * rows - 2, "Invalid data size");
 
-        let mut new_matrix = SparseMatrix {
-            rows,
-            cols: rows,
-            elements: HashMap::new(),
-        };
+        let mut new_matrix = SparseMatrix::new(rows, rows);
 
         for x in 0..rows {
             let val = data[x];
@@ -106,5 +99,38 @@ impl SparseMatrix {
 
         new_matrix
     }
-}
 
+    pub fn add_sparse(&self, matone: &SparseMatrix) -> Option<SparseMatrix> {
+        assert_eq!(
+            self.rows, matone.rows,
+            "Matrix dimensions mismatch"
+        );
+        assert_eq!(
+            self.cols, matone.cols,
+            "Matrix dimensions mismatch"
+        );
+
+        let mut new_matrix = SparseMatrix::new(self.rows, self.cols);
+
+        for (row, col, value) in self.iter_nonzero() {
+            let valueone = matone.get(row, col);
+            let add = value + valueone;
+
+            if add != 0.0 {
+                new_matrix.set(row, col, add);
+            }
+        }
+
+        Some(new_matrix)
+    }
+
+    pub fn transpose_sparse(&self) -> SparseMatrix {
+        let mut new_matrix = SparseMatrix::new(self.cols, self.rows);
+
+        for (row, col, value) in self.iter_nonzero() {
+            new_matrix.set(col, row, value);
+        }
+
+        new_matrix
+    }
+}
